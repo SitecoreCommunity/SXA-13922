415bbe1c2b34182f0c3ef7fc03f2b7f38f9cd8ae
Alen Pelin <alenpelin@gmail.com>
2018-10-01 18:12:17 +1000

    Add default implementation to change later

---
+++ src/SitecoreCommunity.SXA.13922/Foundation/RenderingVariants/Pipelines/RenderVariantField/RenderSection.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore;
+using Sitecore.XA.Foundation.Variants.Abstractions.Pipelines.RenderVariantField;
+
+namespace SitecoreCommunity.SXA.Foundation.RenderingVariants.Pipelines.RenderVariantField
+{
+    public class RenderSection : Sitecore.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField.RenderSection
+    {
+        [UsedImplicitly]
+        public RenderSection()
+        {
+        }
+
+        public override void RenderField(RenderVariantFieldArgs args)
+        {
+            base.RenderField(args);
+        }
+    }
+}
